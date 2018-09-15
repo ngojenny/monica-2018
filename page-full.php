79c155afe603f92706fdfc389863ea963d119888
Jenny <jennyngo.dev@gmail.com>
2018-09-15 12:23:06 -0400

    add package lock, add some basic stylings

--- page-full.php
+++ page-full.php
@@ -10,7 +10,6 @@ get_header();  ?>
   <div class="container">
     <?php // Start the loop ?>
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
       <h2><?php the_title(); ?></h2>
       <?php the_content(); ?>
 
